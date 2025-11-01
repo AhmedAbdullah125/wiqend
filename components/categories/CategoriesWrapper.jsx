c0eac989eb015f1625a97f4fc9ddbe86aa8ce8de
AhmedAbdullah125 <ahmednabeeh98@gmail.com>
2025-11-01 12:40:15 +0200

    update

--- components/categories/CategoriesWrapper.jsx
+++ components/categories/CategoriesWrapper.jsx
@@ -1,5 +1,5 @@
 'use client';
-import React, { useEffect, useState } from 'react';
+import React, { Suspense, useEffect, useState } from 'react';
 import BreadCrumb from '../General/BreadCrumb';
 import ProductsWithFilter from './ProductsWithFilter';
 import { useSearchParams } from 'next/navigation';
@@ -31,12 +31,14 @@ export default function CategoriesWrapper({ type }) {
         }
     })
     return (
-        <section className="content-section">
+        <Suspense fallback={<div>Loading...</div>}>
+            <section className="content-section">
                 <BreadCrumb first={categoryName} firstLink={`/category?category=${categoryId}&name=${categoryName}`} />
-            <div className="container">
-                <ProductsWithFilter loading={loading} data={data} lang={lang} setSortBy={setSortBy} sortBy={sortBy} brand={brand} setBrand={setBrand} tags={tags} setTags={setTags} setPage={setPage} category={category} setCategory={setCategory} prices={prices} setPrices={setPrices} />
-            </div>
-        </section>
+                <div className="container">
+                    <ProductsWithFilter loading={loading} data={data} lang={lang} setSortBy={setSortBy} sortBy={sortBy} brand={brand} setBrand={setBrand} tags={tags} setTags={setTags} setPage={setPage} category={category} setCategory={setCategory} prices={prices} setPrices={setPrices} />
+                </div>
+            </section>
+        </Suspense>
     );
 }
 
